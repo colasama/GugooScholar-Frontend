d288752d60622c6f68255e5232480082da46d136
Colanns <mycolands@gmail.com>
2020-11-20 14:48:46 +0800

    增加了js分包

--- src/vue.config.js
+++ src/vue.config.js
@@ -1,11 +1,41 @@
 // vue.config.js for less-loader@6.0.0
 
 const path = require('path');
+const isProduction = process.env.NODE_ENV !== 'development';
+
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
 module.exports = {
+  productionSourceMap: false,
+  
+  configureWebpack: config => {
+    if (isProduction) {
+      // 开启分离js
+      config.optimization = {
+        runtimeChunk: 'single',
+        splitChunks: {
+          chunks: 'all',
+          maxInitialRequests: Infinity,
+          minSize: 20000,
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name (module) {
+                // get the name. E.g. node_modules/packageName/not/this/part.js
+                // or node_modules/packageName
+                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+                // npm package names are URL-safe, but some servers don't like @ symbols
+                return `npm.${packageName.replace('@', '')}`
+              }
+            }
+          }
+        }
+      }
+    }
+  },
+  
   css: {
     loaderOptions: {
       less: {
