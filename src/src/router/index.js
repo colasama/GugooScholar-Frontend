5194cf32bf61d5a21df86d20a6f6977b65568fe1
prof-H3645 <1009101437@qq.com>
2020-12-03 23:46:13 +0800

    初步完成rank.vue

--- src/src/router/index.js
+++ src/src/router/index.js
@@ -4,70 +4,76 @@ import VueRouter from 'vue-router'
 //import store from '../store/index.js'
 Vue.use(VueRouter)
 
-const routes = [
+  const routes = [
   {
     path: '/',
     name: 'Home',
-    meta: { title: '咕鸽学术 - 学术成果分享平台' },
-    component: () => import('../views/Home.vue')
+    meta:{title:'咕鸽学术 - 学术成果分享平台'},
+    component:() => import('../views/Home.vue')
   },
   {
     path: '/login',
     name: '404',
-    meta: { title: '登录 - 咕鸽学术' },
-    component: () => import('../views/Login.vue')
+    meta: {title:'登录 - 咕鸽学术'},
+    component:() => import('../views/Login.vue')
   },
   {
     path: '/register',
     name: '404',
-    meta: { title: '注册 - 咕鸽学术' },
-    component: () => import('../views/Register.vue')
+    meta: {title:'注册 - 咕鸽学术'},
+    component:() => import('../views/Register.vue')
   },
   {
     // 暂时先这样 测试用 后面具体再改
     path: '/scientist/show',
     name: 'scientist',
-    meta: { title: '科研人员展示 - 咕鸽学术' },
+    meta: {title: '科研人员展示 - 咕鸽学术'},
     component: () => import('../views/content/Scientist')
   },
   {
     path: '/regt',
     name: '404',
-    meta: { title: '注册 - 咕鸽学术' },
-    component: () => import('../views/regTest.vue')
+    meta: {title:'注册 - 咕鸽学术'},
+    component:() => import('../views/regTest.vue')
   },
   {
-    path: '/admin/User',
+    path: '/admin/user',
     name: 'adminUser',
-    meta: { title: '用户管理 - 咕鸽学术' },
-    component: () => import('../views/admin/User.vue')
+    meta: {title: '用户管理 - 咕鸽学术'},
+    component:() => import('../views/admin/User.vue')
   },
   {
     path: '/ranklist',
     name: 'ranklist',
+    meta: {title: '排行榜 - 咕鸽学术'},
+    component:() => import('../views/rank/Ranklist.vue')
+  },
+  {
+    path: '/rank',
+    name: 'Rank',
     meta: { title: '排行榜 - 咕鸽学术' },
-    component: () => import('../views/rank/Ranklist.vue')
+    component: () => import('../views/rank/Rank.vue')
   },
   {
     path: '/search',
     name: 'search',
-    meta: { title: '搜索 - 咕鸽学术' },
-    component: () => import('../views/Search.vue')
+    meta: {title:'搜索 - 咕鸽学术'},
+    component:() => import('../views/Search.vue')
   },
   {
     // 会匹配所有路径
     path: '/paper*',
     name: 'Paper',
-    meta: { title: '论文 - 咕鸽学术' },
-    component: () => import('../views/content/Paper.vue')
+    meta: {title:'论文 - 咕鸽学术'},
+    component:() => import('../views/content/Paper.vue')
   },
-  {
-    // 会匹配所有路径
-    path: '/*',
-    name: '404',
-    meta: { title: '404 - 咕鸽学术' },
-    component: () => import('../views/404.vue')
-  },
+    {
+      // 会匹配所有路径
+      path: '/*',
+      name: '404',
+      meta: {title:'404 - 咕鸽学术'},
+      component:() => import('../views/404.vue')
+    },
 ]
 
 const router = new VueRouter({
---
+++ src/src/views/rank/Rank.vue
@@ -0,0 +1,192 @@
+<template>
+  <a-layout>
+    <a-layout-header class="headtext">
+      <a-icon type="arrow-left" @click="rankList" />&nbsp;&nbsp;{{rankNameList[rankType]}}</a-layout-header>
+    <a-layout-content>
+      <a-table :columns="rankColumns[rankType]" :data-source="rankData[rankType]" class="rank-content" bordered>
+        <span slot="pdf" slot-scope="pdf">
+          <a-button @click="download(pdf)">下载原文</a-button>
+        </span>
+      </a-table>
+      <a-button @click="magic">点击有惊喜</a-button>
+    </a-layout-content>
+  </a-layout>
+</template>
+<script>
+  // @ is an alias to /src
+  export default {
+    name: "Rank",
+    components: {
+
+    },
+    data() {
+      return {
+        rankType: 0,
+        rankNameList: [
+          "科研人员排行榜",
+          "论文排行榜",
+        ],
+        rankColumns: [
+          [{
+              dataIndex: 'name',
+              key: 'name',
+              title: '姓名'
+            },
+            {
+              title: '所属机构',
+              dataIndex: 'orgs',
+              key: 'orgs',
+            },
+            {
+              title: 'H指数',
+              dataIndex: 'h_index',
+              key: 'h_index',
+            },
+            {
+              title: '论文数',
+              key: 'n_pubs',
+              dataIndex: 'n_pubs',
+            },
+            {
+              title: '被引量',
+              key: 'n_citation',
+              dataIndex: 'n_citation'
+            },
+          ],
+          [{
+              title: '标题',
+              dataIndex: 'title',
+              key: 'title',
+            },
+            {
+              title: '作者',
+              dataIndex: 'author',
+              key: 'author',
+            },
+            {
+              title: '来源',
+              dataIndex: 'venue',
+              key: 'venue',
+            },
+            {
+              title: '发表年份',
+              key: 'year',
+              dataIndex: 'year',
+            },
+            {
+              title: '被引量',
+              key: 'n_citation',
+              dataIndex: 'n_citation'
+            },
+            {
+              title: '下载',
+              key: 'pdf',
+              dataIndex: 'pdf',
+              scopedSlots: {
+                customRender: 'pdf'
+              },
+            },
+          ],
+        ],
+        rankData: [
+          [{
+              key: '1',
+              name: 'Donald Trump',
+              orgs: 'White House bunker',
+              h_index: 2000,
+              n_pubs: 200,
+              n_citation: 12,
+            },
+            {
+              key: '2',
+              name: 'Joe Biden',
+              orgs: 'The White House',
+              h_index: 0,
+              n_pubs: 0,
+              n_citation: 0,
+            }, {
+              key: '3',
+              name: '马保国',
+              orgs: '浑元形意太极门',
+              h_index: 2000000,
+              n_pubs: 20,
+              n_citation: 12000000,
+            },
+          ],
+          [{
+              key: '1',
+              title: '论武德',
+              author: "马保国",
+              venue: '不知名期刊',
+              year: 2020,
+              n_citation: 12,
+              pdf: "https://www.baidu.com",
+            },
+            {
+              key: '2',
+              title: '耗子尾汁',
+              author: "马保国",
+              venue: '不知名期刊',
+              year: 2020,
+              n_citation: 12,
+              pdf: "https://www.bilibili.com",
+            },
+            {
+              key: '3',
+              title: '原来是昨天',
+              author: "马保国",
+              venue: '不知名期刊',
+              year: 2020,
+              n_citation: 12,
+              pdf: "https://www.weibo.com",
+            },
+          ],
+
+        ]
+
+      };
+    },
+    watch: {
+
+    },
+    updated() {},
+    mounted() {
+
+    },
+    methods: {
+      download(pdf) {
+        window.open(pdf);
+      },
+      rankList() {
+        this.$router.push("/ranklist");
+      },
+      magic() {
+        this.rankType = 1 - this.rankType;
+      }
+    },
+  };
+</script>
+<style scoped>
+  * {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  .ant-layout .headtext {
+    width: 100%;
+    height: 100px;
+    line-height: 100px;
+    color: #cae8ef;
+    background-color: black;
+    font-size: 30px;
+    font-weight: 500;
+    text-align: left;
+    text-indent: 4em;
+  }
+
+  .ant-layout-content .rank-content {
+    width: 1200px;
+    margin: 0 auto;
+  }
+</style>
