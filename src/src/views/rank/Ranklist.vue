49747b485daf30ace67d0cb6631c9479653d46e3
Rainy-Sky <54521443+Rainy-Sky@users.noreply.github.com>
2020-12-12 17:51:57 +0800

    页面调整+字体悬浮事件

--- src/src/views/rank/Ranklist.vue
+++ src/src/views/rank/Ranklist.vue
@@ -7,57 +7,77 @@
                 </div>
             </a-layout-header>
             <a-layout-content>
-                <a-row type="flex" justify="space-around" align="middle">
-                    <a-col :span="6">
-                        <div class="gutter-box">
-                            col-12
-                        </div>
-                    </a-col>
-                    <a-col :span="6">
-                        <div class="gutter-box">
-                            col-12
-                        </div>
-                    </a-col>
-                    <a-col :span="6">
-                        <div class="gutter-box">
-                            col-12
-                        </div>
-                    </a-col>
-                </a-row>
-                <a-row type="flex" justify="space-around" align="middle">
-                    <a-col :span="6">
-                        <div class="gutter-box">
-                            col-12
-                        </div>
-                    </a-col>
-                    <a-col :span="6">
-                        <div class="gutter-box">
-                            col-12
-                        </div>
-                    </a-col>
-                    <a-col :span="6">
-                        <div class="gutter-box">
-                            col-12
-                        </div>
-                    </a-col>
-                </a-row>
-                <a-row type="flex" justify="space-around" align="middle">
-                    <a-col :span="6">
-                        <div class="gutter-box">
-                            col-12
-                        </div>
-                    </a-col>
-                    <a-col :span="6">
-                        <div class="gutter-box">
-                            col-12
-                        </div>
-                    </a-col>
-                    <a-col :span="6">
-                        <div class="gutter-box">
-                            col-12
-                        </div>
-                    </a-col>
-                </a-row>
+                <div class="place-mid">
+                    <a-row type="flex" justify="space-around" align="middle">
+                        <a-col :span="8">
+                            <div class="gutter-box" id="dcb1">
+                                <div @mouseover="mhover(1,1)" @mouseleave="mleaver(1,1)"  class="in-box" style="font-size: 30px;">
+                                    计算机科学与技术
+                                </div>
+                            </div>
+                        </a-col>
+                        <a-col :span="8">
+                            <div class="gutter-box" id="dcb2">
+                                <div @mouseover="mhover(1,2)" @mouseleave="mleaver(1,2)" class="in-box" style="font-size: 30px;">
+                                    col-12
+                                </div>
+                            </div>
+                        </a-col>
+                        <a-col :span="8">
+                            <div class="gutter-box" id="dcb3">
+                                <div @mouseover="mhover(1,3)" @mouseleave="mleaver(1,3)" class="in-box" style="font-size: 30px;">
+                                    col-12
+                                </div>
+                            </div>
+                        </a-col>
+                    </a-row>
+                    <a-row type="flex" justify="space-around" align="middle">
+                        <a-col :span="8">
+                            <div class="gutter-box"  id="dcb4">
+                                <div @mouseover="mhover(2,1)" @mouseleave="mleaver(2,1)" class="in-box" style="font-size: 30px;">
+                                    col-12
+                                </div>
+                            </div>
+                        </a-col>
+                        <a-col :span="8">
+                            <div class="gutter-box"  id="dcb5">
+                                <div @mouseover="mhover(2,2)" @mouseleave="mleaver(2,2)" class="in-box" style="font-size: 30px;">
+                                    col-12
+                                </div>
+                            </div>
+                        </a-col>
+                        <a-col :span="8">
+                            <div class="gutter-box"  id="dcb6">
+                                <div @mouseover="mhover(2,3)" @mouseleave="mleaver(2,3)" class="in-box" style="font-size: 30px;">
+                                    col-12
+                                </div>
+                            </div>
+                        </a-col>
+                    </a-row>
+                    <a-row type="flex" justify="space-around" align="middle">
+                        <a-col :span="8">
+                            <div class="gutter-box"  id="dcb7">
+                                <div @mouseover="mhover(3,1)" @mouseleave="mleaver(3,1)" class="in-box" style="font-size: 30px;">
+                                    col-12
+                                </div>
+                            </div>
+                        </a-col>
+                        <a-col :span="8">
+                            <div class="gutter-box" id="dcb8">
+                                <div @mouseover="mhover(3,2)" @mouseleave="mleaver(3,2)" class="in-box" style="font-size: 30px;">
+                                    col-12
+                                </div>
+                            </div>
+                        </a-col>
+                        <a-col :span="8">
+                            <div class="gutter-box" id="dcb9">
+                                <div @mouseover="mhover(3,3)" @mouseleave="mleaver(3,3)" class="in-box" style="font-size: 30px;">
+                                    col-12
+                                </div>
+                            </div>
+                        </a-col>
+                    </a-row>
+                </div>
             </a-layout-content>
         </a-layout>
     </div>
@@ -65,7 +85,25 @@
 
 <script>
 export default {
-    
+    data() {
+      return {
+          hover:false
+      }
+    },
+    methods: {
+        mhover(row,col) {
+            console.log(row,col);
+            var num = (row-1)*3 + col;
+            var targetd = document.getElementById("dcb"+num).getElementsByTagName("div")[0];
+            targetd.style.fontSize = "40px";
+        },
+        mleaver(row,col) {
+            console.log(row,col);
+            var num = (row-1)*3 + col;
+            var targetd = document.getElementById("dcb"+num).getElementsByTagName("div")[0];
+            targetd.style.fontSize = "30px";
+        }
+    }
 
 }
 </script>
@@ -93,12 +131,48 @@ export default {
   line-height: 120px;
 }
 .gutter-box {
-    background: rgb(47 50 65 / 88%);
     margin: 10%;
-    padding: 5%;
     height: 200px;
-    font-size: 25px;
-    text-align: left;
+    display: flex;
+}
+#dcb1 {
+    background:lavenderblush;
+}
+#dcb2 {
+    background:rgb(17, 209, 10);
+}
+#dcb3 {
+    background:rgb(233, 50, 5);
+}
+#dcb4 {
+    background:rgb(37, 2, 83);
+}
+#dcb5 {
+    background:rgb(16, 59, 138);
+}
+#dcb6 {
+    background:rgb(199, 228, 35);
+}
+#dcb7 {
+    background:rgb(128, 6, 6);
+}
+#dcb8 {
+    background:rgb(150, 133, 212);
+}
+#dcb9 {
+    background:rgb(165, 47, 155);
+}
+.in-box {
+    height: 100%;
+    width: 100%;
+    opacity: 80%;
+    line-height: 150px;
+    font-family: Roboto-Bold,Roboto;
+    font-weight: 700;
+    transition: all .2s;
+    vertical-align: middle;
+    text-align: center;
+    background-image: url("../../assets/channel_bg.png");
 }
 .ant-col {
     background: #fff;
@@ -106,6 +180,11 @@ export default {
 .ant-row {
     padding: 8px !important;
     margin: auto;
+}
+.place-mid {
+    width: 80%;
+    margin-right: 10%;
+    margin-left: 10%;
 }
 
 </style>
