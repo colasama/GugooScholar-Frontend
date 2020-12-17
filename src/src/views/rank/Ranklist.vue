0d35444f45a83e0ee8822ab71617fb00fb337ca0
Colanns <mycolands@gmail.com>
2020-12-17 22:09:19 +0800

    修改了排行榜列表页面的动画

--- src/src/views/rank/Ranklist.vue
+++ src/src/views/rank/Ranklist.vue
@@ -6,27 +6,19 @@
                     排行榜
                 </div>
             </a-layout-header>
-            <a-layout-content>
-                <div class="place-mid">
-                    <a-row type="flex" justify="space-around" align="middle">
-                        <a-col :span="24">
-                            <div @click="toAuthor" class="gutter-box" id="dcb1">
+            <a-layout-content style="text-align:center">
+                    <div @click="toAuthor" class="gutter-box" id="dcb1">
                                 <div @mouseover="mhover(1,1)" @mouseleave="mleaver(1,1)"  class="in-box box-author" style="font-size: 30px;">
                                     人才热度排行榜
                                 </div>
                             </div>
-                        </a-col>
-                    </a-row>
-                    <a-row type="flex" justify="space-around" align="middle">
-                        <a-col :span="24">
-                            <div @click="toPaper" class="gutter-box"  id="dcb4">
+                            <div @click="toPaper" class="gutter-box" id="dcb4">
                                 <div @mouseover="mhover(2,1)" @mouseleave="mleaver(2,1)" class="in-box box-paper" style="font-size: 30px;">
                                     论文热度排行榜
                                 </div>
                             </div>
-                        </a-col>
-                    </a-row>
-                </div>
+                <div style="margin:0 auto;text-align:center">
+                <div class="box-paper"></div></div>
             </a-layout-content>
         </a-layout>
     </div>
@@ -74,9 +66,6 @@ export default {
 </script>
 
 <style scoped>
-#whole_page {
-  margin-top: 0px !important;
-}
 .ant-layout-footer {
   background: #7dbcea;
   color: #fff;
@@ -94,53 +83,33 @@ export default {
   color: #fff;
   min-height: 120px;
   line-height: 120px;
+  text-align:center;
 }
 .gutter-box {
-    margin: 3%;
+    margin: 48px 0;
     height: 180px;
-    display: flex;
+    width: 1231px;
+    display:inline-block;
 }
-#dcb1 {
-    background:lavenderblush;
-}
-#dcb2 {
-    background:rgb(17, 209, 10);
-}
-#dcb3 {
-    background:rgb(233, 50, 5);
-}
-#dcb4 {
-    background:rgb(37, 2, 83);
-}
+
 .in-box {
-    height: 100%;
-    width: 100%;
-    opacity: 80%;
+    height: 180px;
+    width: 1231px;
+    opacity: 100%;
     padding-left: 5%;
     line-height: 100px;
     font-family: Roboto-Bold,Roboto;
     font-weight: 700;
     transition: all .2s;
-    vertical-align: middle;
     text-align: left;
 }
 .box-paper {
-    background-image: url("../../assets/dark-background-papers.png");
+    background: url("../../assets/dark-background-papers.png");
+    display:inline-block;
 }
 .box-author {
-    background-image: url("../../assets/dark-background-experts.png");
-}
-.ant-col {
-    background: #fff;
-}
-.ant-row {
-    padding: 8px !important;
-    margin: auto;
-}
-.place-mid {
-    width: 80%;
-    margin-right: 10%;
-    margin-left: 10%;
+    background: url("../../assets/dark-background-experts.png");
+    display:inline-block;
 }
 
 </style>
